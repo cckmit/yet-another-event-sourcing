24a42edbc219605822ed181a442a22e1268198e6
Maksim Kostromin <daggerok@gmail.com>
2019-08-26 15:03:50 +0300

    refactor a little bit.... | Done, to whoever merges this, good luck. (C) whatthecommit.com

--- src/test/kotlin/com/github/daggerok/yetanothereventsourcing/user/UserTests.kt
+++ src/test/kotlin/com/github/daggerok/yetanothereventsourcing/user/UserTests.kt
@@ -3,8 +3,13 @@ package com.github.daggerok.yetanothereventsourcing.user
 import com.github.daggerok.yetanothereventsourcing.db.InMemoryEventStore
 import org.assertj.core.api.Assertions.*
 import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
 import java.util.*
 
+@RunWith(SpringRunner::class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class UserTests {
 
     private val eventStore = InMemoryEventStore()
